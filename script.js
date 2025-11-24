2f3f0f6288a1de6db501fc23923c04fd3c6f831c
Javohirdffd <javohirotaboyev111@gmail.com>
2025-11-24 21:31:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function familiyanichiqar() {
+    document.getElementById("familiya").innerText = "OTABOYEV";
+}
+function qoshish() {
+    let a=document.getElementById("son1").value;
+    let b=document.getElementById("son2").value;
+
+    let natija=Number(a)+Number(b);
+    document.getElementById("natija").innerText=natija;
+}
